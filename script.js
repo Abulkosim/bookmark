7178255ea22cc5dbae4133d0aaa00a4bf83ae00d
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2023-06-04 21:31:27 +0500

    js added

---
+++ script.js
@@ -0,0 +1,21 @@
+const spans = Array.from(document.querySelectorAll('.menu span'));
+const tabs = document.querySelectorAll('.tabs');
+const questions = Array.from(document.querySelectorAll('.question'));
+const images = Array.from(document.querySelectorAll('.question img'));
+const answers = Array.from(document.querySelectorAll('.answer'));
+
+spans.forEach(item => item.addEventListener("click", () => {
+  spans.forEach(elem => elem.classList.remove('chosen'));
+  item.classList.add('chosen');
+  tabs.forEach(tab => tab.classList.add('none'));
+  tabs[spans.indexOf(item)].classList.remove('none');
+}))
+
+questions.forEach(item => item.addEventListener("click", () => {
+  images.forEach(image => image.classList.remove('transform'));
+  answers.forEach(answer => answer.classList.add('none'));
+  images.forEach(img => img.src = '/images/icon-arrow.svg');
+  answers[questions.indexOf(item)].classList.remove('none');
+  images[questions.indexOf(item)].classList.add('transform');
+}))
+
